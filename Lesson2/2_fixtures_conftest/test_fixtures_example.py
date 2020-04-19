83505cdf868532d4dfcc01df7ea170a2c061135d
svyazemsky <sema.usu@gmail.com>
2020-04-19 23:54:29 +0500

    lesson 5

--- Lesson2/2_fixtures_conftest/test_fixtures_example.py
+++ Lesson2/2_fixtures_conftest/test_fixtures_example.py
@@ -4,12 +4,12 @@ import pytest
 # Уровень поиска фикстур в файлах conftest
 # В файле теста > в ближайшем conftest
 
-@pytest.fixture
-def first_fixture():
-    print("\nPrint from 'first_fixture'")
+# @pytest.fixture
+# def first_fixture():
+#     print("\nPrint from 'first_fixture'")
 
 
-def test_one(first_fixture):
+def test_one():
     pass
 
 
